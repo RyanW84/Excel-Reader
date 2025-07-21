8750641a0d42704394e1d56da7c045854bcabf85
Ryan W <ryanweavers@gmail.com>
2025-07-21 21:40:45 +0100

    feat(read pdf form): Services and controllers added for reading from a PDF form. It works but the form needs redesigning

---
+++ Services/CreateTableFromPdfForm.cs
@@ -0,0 +1,43 @@
+﻿using System.Data;
+using System.Text;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace ExcelReader.RyanW84.Services;
+
+public class CreateTableFromPdfForm
+{
+    private readonly IConfiguration _configuration;
+
+    public CreateTableFromPdfForm(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public void CreateTableFromPdfFormData(DataTable dataTable)
+    {
+        // Create SQL table and insert data
+        var connectionString = _configuration.GetConnectionString("DefaultConnection");
+        using var connection = new SqlConnection(connectionString);
+        connection.Open();
+
+        // Build CREATE TABLE statement
+        var columnDefs = new List<string>();
+        foreach (DataColumn col in dataTable.Columns)
+        {
+            columnDefs.Add($"[{col.ColumnName}] NVARCHAR(MAX)");
+        }
+        var createTableSql =
+            $"CREATE TABLE [{dataTable.TableName}] ({string.Join(", ", columnDefs)})";
+
+        using (var command = new SqlCommand(createTableSql, connection))
+        {
+            command.ExecuteNonQuery();
+        }
+
+        // Bulk copy the data
+        using var bulkCopy = new SqlBulkCopy(connection);
+        bulkCopy.DestinationTableName = dataTable.TableName;
+        bulkCopy.WriteToServer(dataTable);
+    }
+}
--- Services/ReadFromPdfTable.cs
+++ Services/ReadFromPdfTable.cs
@@ -1,9 +1,8 @@
 using System.Data;
-using System.IO;
-using Microsoft.Extensions.Configuration;
 using iText.Kernel.Pdf;
 using iText.Kernel.Pdf.Canvas.Parser;
 using iText.Kernel.Pdf.Canvas.Parser.Listener;
+using Microsoft.Extensions.Configuration;
 
 namespace ExcelReader.RyanW84.Services;
 
@@ -55,18 +54,17 @@ public class ReadFromPdf(IConfiguration configuration)
         // Join the header row into a single string, then split by '_'
         var headerRow = string.Join(",", pdfData[2]);
         var columnHeaders = headerRow.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-   
 
         foreach (var columnName in columnHeaders)
         {
             dataTable.Columns.Add(columnName.Trim());
         }
 
-       for (int i =3; i<pdfData.Count;i++)
+        for (int i = 3; i < pdfData.Count; i++)
         {
             var dataRow = string.Join(",", pdfData[i]);
-            var readRow = dataRow.Split(' ' , StringSplitOptions.RemoveEmptyEntries);
-           dataTable.Rows.Add(readRow);
+            var readRow = dataRow.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            dataTable.Rows.Add(readRow);
         }
         return dataTable;
     }
