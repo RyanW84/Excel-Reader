24510e1ee6b5da48627071cc82bf2853badbcf6f
Ryan W <ryanweavers@gmail.com>
2025-07-26 21:26:10 +0100

    fix(write controllers): save to db added once write to form has been made

--- Controller/ExcelWriteController.cs
+++ Controller/ExcelWriteController.cs
@@ -1,16 +1,21 @@
 using System.Data;
+using ExcelReader.RyanW84.Data;
 using ExcelReader.RyanW84.Services;
+using Microsoft.Extensions.Configuration;
 
 namespace ExcelReader.RyanW84.Controller;
 
-public class ExcelWriteController(WriteToExcelService writeToExcelService , AnyExcelRead anyExcelRead)
+public class ExcelWriteController(IConfiguration configuration, ExcelReaderDbContext dbContext ,WriteToExcelService writeToExcelService , AnyExcelRead anyExcelRead)
 {
     private readonly WriteToExcelService _writeToExcelService = writeToExcelService;
     private readonly AnyExcelRead _anyExcelRead = anyExcelRead;
+    private readonly IConfiguration _configuration = configuration;
+    private readonly ExcelReaderDbContext _dbContext = dbContext;
 
 	public void WriteDataToExcel(string filePath, Dictionary<string, string> fieldValues)
     {
         _writeToExcelService.WriteFieldsToExcel(filePath, fieldValues);
+        dbContext.SaveChanges();
     }
 
     public Dictionary<string, string> GetExistingFieldValues(string filePath)
--- Controller/PdfFormWriteController.cs
+++ Controller/PdfFormWriteController.cs
@@ -1,20 +1,23 @@
+using ExcelReader.RyanW84.Data;
 using ExcelReader.RyanW84.Services;
+using Microsoft.Extensions.Configuration;
 
 namespace ExcelReader.RyanW84.Controller;
 
-public class PdfFormWriteController
+public class PdfFormWriteController(
+    IConfiguration configuration,
+    ExcelReaderDbContext dbContext,
+    WriteToPdfForm writeToPdfForm)
 {
-    private readonly WriteToPdfForm _writeToPdfForm;
-
-    public PdfFormWriteController(WriteToPdfForm writeToPdfForm)
-    {
-        _writeToPdfForm = writeToPdfForm;
-    }
+    private readonly IConfiguration _configuration = configuration;
+    private readonly ExcelReaderDbContext _dbContext = dbContext;
+    private readonly WriteToPdfForm _writeToPdfForm = writeToPdfForm;
 
     public void WriteDataToPdfForm(string filePath, Dictionary<string, string> fieldValues)
     {
         Console.WriteLine($"Writing data to PDF form: {filePath}");
         _writeToPdfForm.WriteFormFields(filePath, fieldValues);
         Console.WriteLine("PDF form fields updated.");
+        _dbContext.SaveChanges();
     }
 }
